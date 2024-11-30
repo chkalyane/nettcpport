adf32378e177767b63d7227292b8f967972bb020
kk185305 <kk185305@ncrvoyix.com>
2024-11-30 21:09:35 +0530

    test

--- WindowsFormsApplication7mar/ServiceStartForm.Designer.cs
+++ WindowsFormsApplication7mar/ServiceStartForm.Designer.cs
@@ -31,24 +31,25 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(72, 132);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Location = new System.Drawing.Point(63, 56);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(100, 28);
             this.button1.TabIndex = 0;
-            this.button1.Text = "START";
+            this.button1.Text = "NET START";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
             this.button2.Enabled = false;
-            this.button2.Location = new System.Drawing.Point(253, 132);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Location = new System.Drawing.Point(242, 56);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(100, 28);
             this.button2.TabIndex = 1;
@@ -58,7 +59,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(100, 244);
+            this.button3.Location = new System.Drawing.Point(242, 138);
             this.button3.Margin = new System.Windows.Forms.Padding(4);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(100, 28);
@@ -67,15 +68,27 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(63, 138);
+            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 28);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "WebHTTP";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.webhttp_click);
+            // 
             // ServiceStartForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(389, 327);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "ServiceStartForm";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -87,6 +100,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- WindowsFormsApplication7mar/ServiceStartForm.cs
+++ WindowsFormsApplication7mar/ServiceStartForm.cs
@@ -23,32 +23,32 @@ namespace WindowsFormsApplication7mar
             InitializeComponent();
         }
 
+        //NetTCP
         private void button1_Click(object sender, EventArgs e)
         {
             host1 = new ServiceHost(typeof(implementclass));
-            WebHttpBinding binding1 = new WebHttpBinding();
-            binding1.CrossDomainScriptAccessEnabled = true;
-            binding1.Security.Mode = WebHttpSecurityMode.Transport;
-            binding1.Security.Transport.ClientCredentialType = HttpClientCredentialType.Certificate;
-            ServiceMetadataBehavior sb = new ServiceMetadataBehavior();
-            sb.HttpsGetEnabled = true;
-            sb.HttpsGetUrl = new Uri("https://localhost:443/implementclass");
-            host1.Description.Behaviors.Add(sb);
-            //binding1.Security.Transport = new HttpTransportSecurity() { ClientCredentialType = HttpClientCredentialType.Certificate };
-            // binding1.Security.Message.ClientCredentialType = MessageCredentialType.Certificate;
-            //binding1.PortSharingEnabled = true;
-            host1.AddServiceEndpoint(typeof(myinterface), binding1, new Uri("https://localhost:8080/implementclass"));
+            NetTcpBinding binding1 = new NetTcpBinding(SecurityMode.None);
+            binding1.PortSharingEnabled = true;
+
+            host1.AddServiceEndpoint(typeof(myinterface), binding1, new Uri("net.tcp://localhost:5000/implementclass"));
+
+
             host1.Open();
-            //host2 = new ServiceHost(typeof(secondClass));
-            //NetTcpBinding binding2 = new NetTcpBinding( SecurityMode.None);
-            //binding2.PortSharingEnabled = true;
-            //host2.AddServiceEndpoint(typeof(MySecondInterface), binding2, new Uri("https://localhost:8080/secondClass"));
-            //host2.Open();
-            //MessageBox.Show("started two services");
+            host2 = new ServiceHost(typeof(secondClass));
+            NetTcpBinding binding2 = new NetTcpBinding(SecurityMode.None);
+            binding2.PortSharingEnabled = true;
+
+
+            host2.AddServiceEndpoint(typeof(MySecondInterface), binding2, new Uri("net.tcp://localhost:5000/secondClass"));
+
+
+            host2.Open();
+            MessageBox.Show("started two services");
             button1.Enabled = false;
             button2.Enabled = true;
         }
 
+        //client
         private void button3_Click(object sender, EventArgs e)
         {
             var httpBinding = new WebHttpBinding();
@@ -62,7 +62,7 @@ namespace WindowsFormsApplication7mar
 
 
             var factory = new ChannelFactory<myinterface>(httpBinding, httpEndpoint);
-            X509Certificate2 trustedCertificate = new X509Certificate2(@"C:\Users\kk185305\Downloads\wcfnettcp (1)\wcfnettcp\wcfnettcp\WindowsFormsApplication7mar\WindowsFormsApplication7mar\newapstest.pfx", "test1@3"); //SSL
+            X509Certificate2 trustedCertificate = new X509Certificate2(@"..\newapstest.pfx", "test1@3"); //SSL
             factory.Credentials.ClientCertificate.Certificate = trustedCertificate;
             //var behaviour = new WebHttpBehavior();
             ////behaviour.AddBindingParameters. = new Uri("https://localhost:8080/implementclass");
@@ -71,6 +71,24 @@ namespace WindowsFormsApplication7mar
 
             var proxy = factory.CreateChannel();
                 proxy.Upload("asdf");
+        }
+
+        private void webhttp_click(object sender, EventArgs e)
+        {
+            host1 = new ServiceHost(typeof(implementclass));
+            WebHttpBinding binding1 = new WebHttpBinding();
+            binding1.CrossDomainScriptAccessEnabled = true;
+            binding1.Security.Mode = WebHttpSecurityMode.Transport;
+            binding1.Security.Transport.ClientCredentialType = HttpClientCredentialType.Certificate;
+            ServiceMetadataBehavior sb = new ServiceMetadataBehavior();
+            sb.HttpsGetEnabled = true;
+            sb.HttpsGetUrl = new Uri("https://localhost:443/implementclass");
+            host1.Description.Behaviors.Add(sb);
+            host1.AddServiceEndpoint(typeof(myinterface), binding1, new Uri("https://localhost:443/implementclass"));
+            host1.Open();
+            MessageBox.Show("service is started");
+            button1.Enabled = false;
+            button2.Enabled = true;
         }
 
         private void button2_Click(object sender, EventArgs e)
